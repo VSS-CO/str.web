99524eebd6057a1e034da62f26ee8103cac5feee
VSSCO <161714242+kessud2021@users.noreply.github.com>
2026-01-06 18:24:56 +0530

    Create dom.js

---
+++ src/utils/dom.js
@@ -0,0 +1,6 @@
+export function createElement(tag, className = '', innerHTML = '') {
+    const el = document.createElement(tag);
+    if (className) el.className = className;
+    if (innerHTML) el.innerHTML = innerHTML;
+    return el;
+}
