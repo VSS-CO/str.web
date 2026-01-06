96ea5d863876017e0e8919647f7b4554f3fd2fb1
VSSCO <161714242+kessud2021@users.noreply.github.com>
2026-01-06 18:25:39 +0530

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+export function createFooter() {
+    const footer = document.createElement('footer');
+    footer.innerHTML = `
+        <p>© 2026 Strata Extended. All rights reserved.</p>
+    `;
+    return footer;
+}
